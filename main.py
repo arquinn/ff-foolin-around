99827bd6abacd25b27968d15daed4d0d8e33e5a3
Andrew Quinn <arquinn@umich.edu>
2019-10-01 18:25:36 -0400

    fixed up how I calc expected wins for pre 2018

--- main.py
+++ main.py
@@ -19,6 +19,34 @@ def get_name(team):
         return team.team_abbrev
     return "".join([word[0] for word in team.owner.split()])
 
+def expected_wins_by_team(league):
+    teams = league.teams
+    df = DataFrame(columns=["Points", "Win", "Week", "Team"])
+    for team in teams:
+        games = [[data[0], data[1], week, get_name(team)] for week,data in enumerate(zip(team.scores,team.won_games))]
+        teamDF = DataFrame(games, columns=["Points", "Win", "Week", "Team"])
+        # teamDF['Win'] = df['Win'].astype(int)
+
+        # assume that a 0.0 as a score implies that the game hasn't happened
+        teamDF = teamDF.loc[teamDF["Points"] > 0]
+        df = df.append(teamDF)
+    df = df.reset_index(drop=True)
+
+    exp_wins = df.groupby(by="Week")["Points"]\
+                 .rank(ascending=True)
+                 # .rename(columns={"Points":"Place"})
+
+    df = df.join(exp_wins, lsuffix="orig",rsuffix="ew")
+    df.rename(columns={"Pointsew":"Rank", "Pointsorig":"Points"}, inplace=True)
+    df["Exp Win"] = df["Rank"] / (len(league.teams) - 1)
+    df["Win"] = df["Win"].astype(int)
+
+    df = df.filter(["Team","Exp Win","Win"])\
+           .groupby(by="Team")\
+           .agg({"Exp Win": "sum", "Win": "sum"})
+    return df
+
+
 def expected_wins(league, weeks):
     allWeeks = DataFrame(columns=["Team","Win","Exp Win"])
     for week in weeks:
@@ -27,9 +55,9 @@ def expected_wins(league, weeks):
         data = []
         for m in scores:
             if m.home_team:
-                data.append([week, 
+                data.append([week,
                              get_name(m.home_team),
-                             m.home_score, 
+                             m.home_score,
                              m.home_score > m.away_score])
             if m.away_team:
                 data.append([week,
@@ -99,15 +127,13 @@ def main():
                     config['LEAGUE_INFO']['s2'],\
                     config['LEAGUE_INFO']['SWID'])
 
+    week = args.week
+    if week == 0:
+        week = league.nfl_week
 
-    if args.week == 0:
-        
-        print("On week %d" %(league.current_week))
-        ewins_df = expected_wins(league, range(1, league.nfl_week + 1))
-    else:
-        ewins_df = expected_wins(league, range(1, args.week + 1))
+    print("On week %d" %(league.current_week))
+    ewins_df = expected_wins_by_team(league)
     draw_agg_dataframe(ewins_df)
-
 
 
 if __name__ == "__main__":
